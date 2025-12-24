cf3ea5786ed3683fc143c639741bbfd9b03733b7
José Vitor <jose.vitorw018@gmail.com>
2025-12-24 10:03:31 -0300

    Alterando o usuario

--- src/main/java/teste/basico/AlterarUsuario1.java
+++ src/main/java/teste/basico/AlterarUsuario1.java
@@ -1,4 +1,26 @@
 package teste.basico;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import modelo.basico.Usuario;
+
 public class AlterarUsuario1 {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("meuPU");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Usuario usuario = em.find(Usuario.class, 2l);
+        usuario.setNome("Leonardo");
+        usuario.setEmaiil("leonardo@lanche.com.br");
+
+        em.merge(usuario);
+
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+    }
 }
